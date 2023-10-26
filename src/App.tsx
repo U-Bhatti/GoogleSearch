8ff162170ae111fbc7778b93fa4318af8424f6d3
Umair Bhatti <umair.bhatti@makeitfly.group>
2023-10-26 04:33:46 +0200

    Starter code

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <ul className='flex gap-3'>
+        <li>Gmail</li>
+        <li>Afbeelding</li>
+        <li>Google Apps Icon</li>
+        <li>Google Profile Icon</li>
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
